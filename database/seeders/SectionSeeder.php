8e2e82b4d83abef5d5aa18af2e64d99b04fa138a
Masirika Constantin <elconstant73@gmail.com>
2024-12-29 23:51:15 +0100

    img1

--- database/seeders/SectionSeeder.php
+++ database/seeders/SectionSeeder.php
@@ -16,6 +16,6 @@ class SectionSeeder extends Seeder
         }
 
         // Créez des sections liées aux posts existants
-        Section::factory(50)->create(); // Génère 50 sections aléatoires
+        Section::factory(150)->create(); // Génère 50 sections aléatoires
     }
 }
