c81ef31df5e9f55b28cfd3234595f145d8126538
Masirika Constantin <elconstant73@gmail.com>
2025-01-05 20:54:04 +0100

    coco222

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::with(['type', 'categorie', 'user'])->latest()->paginate(10);
+        $posts = Post::with(['type', 'categorie', 'user'])->latest()->paginate(100);
         return view('admin.posts.index', compact('posts'));
     }
 
