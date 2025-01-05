03b711c363a694506670a13f79a353c30ce41986
Masirika Constantin <elconstant73@gmail.com>
2025-01-05 11:46:16 +0100

    Commentaire seed

---
+++ database/factories/CommentaireFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Commentaire>
+ */
+class CommentaireFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'post_id' => Post::inRandomOrder()->value('id') ?? Post::factory(), // ID valide ou nouveau Post
+            'user_id' => User::inRandomOrder()->value('id') ?? User::factory(), // ID valide ou nouveau Post
+            'contenu' => $this->faker->paragraphs(3, true), // Contenu en paragraphes
+            'created_at' => $this->faker->dateTimeBetween('-1 year', 'now'), // Date de création
+            'updated_at' => $this->faker->dateTimeBetween('-6 months', 'now'), // Dernière mise à jour
+        
+        ];
+    }
+}
---
+++ database/seeders/CommentaireSeed.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Commentaire;
+use App\Models\Post;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CommentaireSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        /// Assurez-vous que des posts existent
+        if (Post::count() === 0) {
+            \App\Models\Post::factory(10)->create(); // Génère des posts si nécessaires
+        }
+
+        // Créez des sections liées aux posts existants
+        Commentaire::factory(150)->create(); // Génère 50 sections aléatoires
+    }
+}
