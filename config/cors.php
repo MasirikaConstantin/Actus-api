5734f1c91a120ce72314ef69c623e26de2fe2c15
Masirika Constantin <elconstant73@gmail.com>
2025-05-04 11:15:46 +0100

    sure

---
+++ config/cors.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://deluxe-coco.netlify.app', // ajoute ici tous les domaines autorisés
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true, // 🚨 Obligatoire avec withCredentials
+
+];
